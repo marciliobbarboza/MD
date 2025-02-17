0a45bd520529cfd0fa259afef02daab0778db567
Pedr0Hc <pedro800go@gmail.com>
2025-02-17 16:16:05 -0400

    Remove .env file from repository and add to .gitignore

--- More or Less/models/moviesTR.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const movieSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    rating: { type: Number, required: true, min: 0, max: 5 },
-});
-
-module.exports = mongoose.model("Movie", movieSchema);
