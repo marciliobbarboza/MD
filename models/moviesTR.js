1db42da6089dc5fc3aa03a64228bc9d6d1ad9c1f
Pedr0Hc <pedro800go@gmail.com>
2025-02-17 15:28:15 -0400

    Adding the genre search

--- models/moviesTR.js
+++ models/moviesTR.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const movieSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    rating: { type: Number, required: true, min: 0, max: 5 },
+    genre: [{ type: String, required: true }],
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    releaseYear: Number,
 });
 
 module.exports = mongoose.model("Movie", movieSchema);
